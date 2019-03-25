19b7573e089fda700db24b7a4c8ee613e5d67589
DavidMaldonado0 <eduardodavidmm@hotmail.com>
2019-03-24 22:20:43 -0600

    add form

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Todos from './components/Todos';
+import Header from './components/layout/Header'
+import AddTodo from './components/layout/AddTodo'
 
 class App extends Component {
 
@@ -43,6 +45,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
+        <AddTodo />
         <Todos todos={this.state.todos} markComplete={this.markComplete}
         delTodo={this.delTodo} />
       </div>
---
+++ todoapp/src/components/layout/AddTodo.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function AddTodo() {
+    return(
+        <form style={{ display: 'flex' }}> 
+            <input 
+                type="text" 
+                name="title" 
+                style={{ flex: '10', padding: '5px'}}
+                placeholder="Add ToDo..."
+            />
+            <input 
+                type="submit" 
+                value="Submit"
+                className="btn"
+                style={{flex: '1'}}
+            />
+        </form>
+    )
+}
+
+
+
+export default AddTodo;
